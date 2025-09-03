c36387ed1aabb2898b90cce9b31fc90ec960452b
RBXSL <sanabriajacob11@gmail.com>
2025-09-02 23:01:39 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,27 @@
 import os
+import discord
 from discord.ext import commands
 
-BOT_TOKEN = os.getenv("DISCORD_TOKEN")  # pulls from Render environment
+# ---------- CONFIG ----------
+BOT_TOKEN = os.getenv("DISCORD_TOKEN")  # Make sure this is set in Render environment variables
+# ----------------------------
 
-intents = commands.Intents.all()  # or whatever intents you want
-bot = commands.Bot(command_prefix="!", intents=intents)
+# Set intents
+intents = discord.Intents.default()
+intents.guilds = True
+intents.members = True
+intents.message_content = True
+
+# No prefix commands
+bot = commands.Bot(command_prefix="", intents=intents)
 
 @bot.event
 async def on_ready():
-    print(f"Logged in as {bot.user}")
+    print(f"Bot is online as {bot.user}!")
+
+# Example command without prefix
+@bot.command()
+async def ping(ctx):
+    await ctx.send("Pong!")
 
 bot.run(BOT_TOKEN)
